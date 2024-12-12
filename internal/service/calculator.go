b55f322db57489f0ea13084745d7526a9c013e8e
pvdevs <paulovictor.devs@gmail.com>
2024-12-12 05:38:03 -0300

    remove unused code from calculator

--- internal/service/calculator.go
+++ internal/service/calculator.go
@@ -29,7 +29,6 @@ func NewCalculator(client StarshipClient) *Calculator {
 
 // CalculateStops determines how many stops each starship needs to make for a given distance
 // It returns a map of starship names to their required number of stops
-// CalculateStops determines how many stops each starship needs to make for a given distance
 func (c *Calculator) CalculateStops(ctx context.Context, distance int64) (map[string]int, error) {
 	starships, err := c.client.GetStarships(ctx)
 	if err != nil {
@@ -59,30 +58,4 @@ func (c *Calculator) CalculateStops(ctx context.Context, distance int64) (map[st
 	}
 
 	return results, nil
-}
-
-// Helper method to calculate stops for a single starship
-// This is useful when we want to calculate stops for specific ships
-func (c *Calculator) calculateStopsForShip(shipName string, MGLT int, consumables string, distance int64) (int, error) {
-	// Skip ships that can't move
-	if MGLT <= 0 {
-		return 0, nil
-	}
-
-	// Parse consumables to get hours of travel time
-	hours, err := parser.ParseConsumables(consumables)
-	if err != nil {
-		return 0, fmt.Errorf("parse consumables: %w", err)
-	}
-
-	// Calculate maximum distance ship can travel before needing to stop
-	maxDistance := MGLT * hours
-
-	// Calculate number of stops needed
-	stops := int(distance) / maxDistance
-	if int(distance)%maxDistance == 0 && stops > 0 {
-		stops--
-	}
-
-	return stops, nil
 }
